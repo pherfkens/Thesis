4716e141216bf537db0351c950e20009b51b7196
pherfkens <159798526+pherfkens@users.noreply.github.com>
2025-06-14 21:43:21 +0200

    Delete py_files directory

--- py_files/model.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-from transformers import ASTConfig, ASTForAudioClassification
-
-def get_model(pretrained_model, num_outputs=8):
-    config = ASTConfig.from_pretrained(pretrained_model)
-    model = ASTForAudioClassification.from_pretrained(pretrained_model, config=config, ignore_mismatched_sizes=True)
-    model.classifier = nn.Linear(config.hidden_size, num_outputs)
-    model.init_weights()
-    return model
